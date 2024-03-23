f4f352873c3bcb9d2676a1c3a245e9256defe75a
jihyelee9804 <jihyelee9804@gmail.com>
2024-03-23 20:44:17 +0900

    add create todo, add read todo

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,14 +1,14 @@
 import "./TodoList.css";
 import TodoItem from "./TodoItem";
 
-const TodoList = () => {
+const TodoList = ({ todo }) => {
     return <div className="TodoList">
         <h4>Todo List 🌱</h4>
         <input className="searchbar" placeholder="검색어를 입력하세요" />
         <div className="list_wrapper">
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+            {todo.map((it) => (
+                <TodoItem {...it} />
+            ))}
         </div>
     </div>
 };
