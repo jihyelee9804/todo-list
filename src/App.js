7d67a0309a6463a22d8d31a3d7db5ea10f63095d
jihyelee9804 <jihyelee9804@gmail.com>
2024-03-25 15:25:45 +0900

    use useReducer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './component/Header';
 import TodoEditor from './component/TodoEditor';
 import TodoList from './component/TodoList';
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import { useRef } from "react";
 const mockTodo = [
   {
@@ -25,25 +25,68 @@ const mockTodo = [
   },
 ];
 
-function App() {
-  const idRedf = useRef(3);
-  const [todo, setTodo] = useState(mockTodo);
-  const onCreate = (content) => {
-    const newItem = {
-      id: idRedf.current,
-      content,
-      isDone: false,
-      createdDate: new Date().getTime(),
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE": {
+      // 기존 할일 아이템에 action 객체의 아이템이 추가된 새 배열을 반환한다.
+      return [action.newItem, ...state];
+    }
+    case "UPDATE": {
+      return state.map((it) =>
+      it.id === action.targetId ? {
+        ...it,
+        isDone: !it.isDone,
+      }
+      : it
+      )
     };
-    setTodo([newItem, ...todo]);
-    idRedf.current += 1;
+    case "DELETE": {
+      return state.filter((it) => it.id !== action.targetId);
+    }
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [todo, dispatch] = useReducer(reducer, mockTodo);
+  const idRef = useRef(3);
+
+  // TodoItem을 추가하는 기능
+  const onCreate = (content) => {
+    dispatch({
+      type: "CREATE",
+      newItem: {
+        id: idRef.current,
+        content,
+        isDone: false,
+        createdDate: new Date().getTime(),
+      },
+    });
+    idRef.current += 1;
+  };
+
+// TodoItem 체크박스에 틱이 발생했을 때 isDone 프로퍼티 값을 토글하는 기능
+  const onUpdate = (targetId) => {
+    dispatch({
+      type: "UPDATE",
+      targetId,
+    });
+  };
+  
+// TodoItem 삭제하는 기능
+  const onDelete = (targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId,
+    });
   };
 
   return (
     <div className="App">
       <Header />
       <TodoEditor onCreate={onCreate} />
-      <TodoList todo={todo}/>
+      <TodoList todo={todo} onUpdate={onUpdate} onDelete={onDelete}/>
     </div>
   );
 }
--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,16 +1,37 @@
 import "./TodoList.css";
 import TodoItem from "./TodoItem";
+import { useState } from "react";
+const TodoList = ({ todo, onUpdate, onDelete }) => {
+    const [search, setSearch] = useState("");
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value);
+    }
+    const getSearchResult = () => {
+        return search === ""
+        ? todo
+        : todo.filter((it) => it.content.toLowerCase().includes(search.toLowerCase()));
+    }
 
-const TodoList = ({ todo }) => {
-    return <div className="TodoList">
-        <h4>Todo List 🌱</h4>
-        <input className="searchbar" placeholder="검색어를 입력하세요" />
-        <div className="list_wrapper">
-            {todo.map((it) => (
-                <TodoItem {...it} />
-            ))}
+    return (
+        <div className="TodoList">
+            <h4>Todo List 🌱</h4>
+            <input 
+            value={search}
+            onChange={onChangeSearch}
+            className="searchbar"
+            placeholder="검색어를 입력하세요"
+            />
+            <div className="list_wrapper">
+                {getSearchResult().map((it) => (
+                    <TodoItem key={it.id}
+                    {...it}
+                    onUpdate={onUpdate}
+                    onDelete={onDelete}
+                    />
+                ))}
+            </div>
         </div>
-    </div>
+    );
 };
 
 export default TodoList;
